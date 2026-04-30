30b8b20e8a22106f559d9558cb70cc7aaa8bbadf
ernapeters0 <144230882+ernapeters0@users.noreply.github.com>
2026-04-30 02:22:12 -0700

    Update spfiles.sh

--- spfiles.sh
+++ spfiles.sh
@@ -20,7 +20,22 @@ for file in "$TARGET_DIR"/*.mp4; do
     if [ "$size" -gt "$limit" ]; then
         echo "Splitting '$file'..."
         # ffmpeg splits and keeps parts in the same folder as the source
-        ffmpeg -i "$file" -fs 64000000 -c copy "${file%.mp4}_part%03d.mp4"
+        SIZELIMIT="99000000"
+        DURATION=$(ffprobe -i "$file" -show_entries format=duration -v quiet -of default=noprint_wrappers=1:nokey=1|cut -d. -f1)
+        CUR_DURATION=0
+        BASENAME="${file%.*}"
+        EXTENSION="mp4"
+        i=1
+        NEXTFILENAME="$BASENAME-$i.$EXTENSION"
+        echo "Duration of source video: $DURATION"
+        while [[ $CUR_DURATION -lt $DURATION ]]; do
+            ffmpeg -ss "$CUR_DURATION" -i "$file" -fs "$SIZELIMIT" -c copy "$NEXTFILENAME"
+            NEW_DURATION=$(ffprobe -i "$NEXTFILENAME" -show_entries format=duration -v quiet -of default=noprint_wrappers=1:nokey=1|cut -d. -f1)
+            CUR_DURATION=$((CUR_DURATION + NEW_DURATION))
+            i=$((i + 1))
+            echo "Part No. $i starts at $CUR_DURATION"
+            NEXTFILENAME="$BASENAME-$i.$EXTENSION"
+        done
         rm "$file" && echo "Successfully split and deleted: $(basename "$file")"
     else
         echo "Skipping '$(basename "$file")' (under 100MB)"
